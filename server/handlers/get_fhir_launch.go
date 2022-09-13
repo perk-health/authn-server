98270ba2448669497254587179b9a73f33742191
Doug DeBold <ddebold56@gmail.com>
2022-09-13 14:30:09 -0500

    Cleanup FHIR Launch Handler

--- server/handlers/get_fhir_launch.go
+++ server/handlers/get_fhir_launch.go
@@ -2,54 +2,73 @@ package handlers
 
 import (
 	"encoding/base64"
+	"errors"
 	"net/http"
 	"net/url"
 
 	"github.com/keratin/authn-server/app"
 	"github.com/keratin/authn-server/app/tokens/oauth"
 	"github.com/keratin/authn-server/lib"
+	"github.com/keratin/authn-server/lib/route"
+	"github.com/keratin/authn-server/lib/smart_on_fhir"
 )
 
 func GetFhirLaunch(app *app.App, providerName string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		redirectURI := app.Config.AuthNURL.String() + "/fhir/" + providerName + "/return"
 		provider := app.SmartOnFhirProviders[providerName]
 		issuer := r.FormValue("iss")
 		launch := r.FormValue("launch")
+		redirectURI := r.FormValue("redirect_uri")                                           // redirect URI to the UI
+		authnReturnUri := app.Config.AuthNURL.String() + "/fhir/" + providerName + "/return" // Authn to return to after login
 
-		// Not currently used, but could be:
-		// metadata := new(OpenIDConfiguration)
-		// discoverAuthServer(issuer, metadata)
+		if route.FindDomain(redirectURI, app.Config.ApplicationDomains) == nil {
+			app.Reporter.ReportRequestError(errors.New("unknown redirect domain"), r)
+			failsafe := app.Config.ApplicationDomains[0].URL()
+			http.Redirect(w, r, failsafe.String(), http.StatusSeeOther)
+			return
+		}
+
+		// fail handler
+		fail := func(err error) {
+			app.Reporter.ReportRequestError(err, r)
+			redirectFailure(w, r, redirectURI)
+		}
 
 		// set nonce in a secured cookie
 		bytes, err := lib.GenerateToken()
 		if err != nil {
-			WriteData(w, http.StatusBadRequest, map[string]interface{}{"error": err.Error()})
+			fail(err)
+			return
 		}
 		nonce := base64.StdEncoding.EncodeToString(bytes)
 		http.SetCookie(w, nonceCookie(app.Config, string(nonce)))
 
 		// save nonce and return URL into state param
-		// TODO: configure and store the URL of the application to redirect to
-		// after the authorization process is complete. This is NOT the AuthN Server URL,
-		// but the URL of the application
 		stateToken, err := oauth.New(app.Config, string(nonce), redirectURI)
 		if err != nil {
-			WriteData(w, http.StatusBadRequest, map[string]interface{}{"error": err.Error()})
+			fail(err)
+			return
 		}
 		state, _ := stateToken.Sign(app.Config.OAuthSigningKey)
 
 		// Create the URL to redirect to
-		finalRedirectUrl, _ := url.Parse(provider.Config(redirectURI).AuthCodeURL(state))
-
-		// Add the launch and aud parameters to the URL
-		// http://www.hl7.org/fhir/smart-app-launch/app-launch.html#request-4
-		values := finalRedirectUrl.Query()
-		values.Add("launch", launch)
-		values.Add("aud", issuer)
-		finalRedirectUrl.RawQuery = values.Encode()
+		// returnUrl, _ := buildAuthnReturnUrl(authnReturnUri, provider, state, launch, issuer)
+		returnUrl, _ := buildAuthnReturnUrl(authnReturnUri, provider, state, launch, issuer)
 
 		// Redirect user to the final URL
-		http.Redirect(w, r, finalRedirectUrl.String(), http.StatusSeeOther)
+		http.Redirect(w, r, returnUrl.String(), http.StatusSeeOther)
 	}
+}
+
+func buildAuthnReturnUrl(baseUri string, provider smart_on_fhir.FhirProvider, state string, launch string, issuer string) (*url.URL, error) {
+	returnUrl, err := url.Parse(provider.Config(baseUri).AuthCodeURL(state))
+
+	// Add the launch and aud parameters to the URL
+	// http://www.hl7.org/fhir/smart-app-launch/app-launch.html#request-4
+	values := returnUrl.Query()
+	values.Add("launch", launch)
+	values.Add("aud", issuer)
+	returnUrl.RawQuery = values.Encode()
+
+	return returnUrl, err
 }
